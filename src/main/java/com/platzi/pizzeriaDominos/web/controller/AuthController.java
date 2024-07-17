0d436457eb8eb2a71830a1598f730acab007bb1b
Helen Acero <hepacest@gmail.com>
2024-07-17 12:17:51 -0500

    Code where JWT was integrated

--- src/main/java/com/platzi/pizzeriaDominos/web/controller/AuthController.java
+++ src/main/java/com/platzi/pizzeriaDominos/web/controller/AuthController.java
@@ -4,8 +4,6 @@ import com.platzi.pizzeriaDominos.service.DTO.LoginDto;
 import com.platzi.pizzeriaDominos.web.config.JwtUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
